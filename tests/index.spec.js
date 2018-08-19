d2beae69b44395c27108ecb9267e63923a130c53
Harry Blum <himion0@gmail.com>
2018-08-19 17:15:28 +0100

    Better test description

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -2,7 +2,7 @@ const { test } = require("ava");
 const { app } = require("../src/index");
 const { request } = require("chai").use(require("chai-http"));
 
-test("We recieve message", async t => {
+test("We recieve the correct message", async t => {
   let res = await request(app).get("/").send();
   t.is(res.body.message, "HEY!");
   t.pass()
